b8f00e786e15583d5686e0e95ad2c7b69e27e0f6
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-04-14 11:06:24 -0400

    break tags

--- src/lib/fiff/include/fiff/tag.hpp
+++ src/lib/fiff/include/fiff/tag.hpp
@@ -12,9 +12,27 @@
 
 namespace Fiff {
 
-struct Tag {
-  Kind kind;   // Tag number.
-  Type type;   // Data type.
+class Tag {
+public:
+  Kind getKind();
+  void setKind(Kind);
+  Type getType();
+  void setType(Type);
+  void setType(int);
+
+  TypeFormat getTypeFormat();
+  void setTypeFormat(TypeFormat);
+  TypeRepresentation getTypeRepresentation();
+  void setTypeRepresentation(TypeRepresentation);
+  int getSize();
+  void setSize(int);
+
+  Data& getData();
+  void setData(Data);
+
+private:
+  int kind;   // Tag number.
+  int type;   // Data type.
   int32_t size;   // Size of data.
   int32_t next;   // Next object. 0 is sequential, -1 if end of file.
   Data data;   // Pointer to data.
